62a3e2a64f0fd775315ccffe2a6f1027b57d246a
Andreas Gejl Madsen <andreasgejlm@gmail.com>
2019-12-27 21:17:32 +0100

    Working prototype

---
+++ analysis.py
@@ -0,0 +1,231 @@
+from parse import *
+import matplotlib.pyplot as plt
+import numpy as np
+from wordcloud import WordCloud
+from matplotlib.dates import date2num, num2date
+from matplotlib import dates as dt
+import seaborn as sns
+import sys
+import pytz
+import subprocess
+from PIL import Image
+from io import BytesIO
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.platypus import Paragraph, Frame
+from PyPDF2 import PdfFileWriter, PdfFileReader
+from reportlab.lib.styles import ParagraphStyle
+
+DIR = os.getcwd() + "/messages/inbox/"
+new_image_dir = os.getcwd() + "/Images/"
+
+class Analysis:
+    def __init__(self):
+        self.heatmapHeight = 0
+        self.wordHeight = 0
+        self.pageWidth = 1800
+        self.basePageHeight = 1500
+        self.figureWidthInches = 18
+        self.figureHeightInches = self.figureWidthInches / 3
+
+        self.figureWidthPix = self.figureWidthInches * 100
+        self.figureHeightPix = self.figureHeightInches * 100
+
+    def getConversations(self):
+        conversation_files = os.listdir(DIR)
+        conversation_files.sort()
+        return conversation_files
+
+    def createScatterChart(self):
+        print("Creating line chart")
+        dates = self.p.get_line_chart_data()
+        fig, ax = plt.subplots(1, 1, figsize=(self.figureWidthInches, self.figureHeightInches))
+
+        for user in dates:
+            y = list(dates[user].values())
+            x = [date2num(date) for date in list(dates[user].keys())]
+            plt.scatter(x, y, label=user, s=10)
+            ax.xaxis.set_major_formatter(dt.DateFormatter('%b %d, %Y'))
+
+        plt.gca().spines["top"].set_alpha(.3)
+        plt.gca().spines["bottom"].set_alpha(.3)
+        plt.gca().spines["right"].set_alpha(.3)
+        plt.gca().spines["left"].set_alpha(.3)
+
+        ax.set_xlabel('Date', fontsize=20)
+        ax.set_ylabel('Number of messages', fontsize=20)
+        ax.grid(alpha=.4)
+
+        plt.title("All messages sent in conversation history", fontsize=30)
+        plt.legend()
+        plt.savefig(new_image_dir + 'line_history.png', dpi=400)
+        plt.clf()
+        #plt.show()
+
+    def createHeatmap(self):
+        print("Creating heatmap")
+        hour_index = [h for h in range(23)]
+        day_index = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        days = self.p.get_heatmap_data()
+        fig = plt.figure(figsize=(self.figureWidthInches, self.figureHeightInches * len(days)))
+        for i, user in enumerate(days):
+            ax = fig.add_subplot(len(days), 1, i + 1)
+            sns.heatmap(days[user], cmap='Blues', linewidths=2, yticklabels=day_index)
+            ax.set_title("Hourly heatmap of messages sent by " + user, fontsize=30)
+            self.heatmapHeight += self.figureHeightPix
+        plt.tight_layout()
+        plt.savefig(new_image_dir + 'hourly_heatmap.png', dpi=400)
+        plt.clf()
+
+    def createWordcloud(self):
+        print("Creating wordcloud")
+        messages = self.p.get_all_messages_for_wordcloud()
+        fig = plt.figure(figsize=(self.figureWidthInches, self.figureHeightInches * len(messages)))
+        for i, user in enumerate(messages):
+            ax = fig.add_subplot(len(messages), 1, i + 1)
+            wc = WordCloud(background_color='white', width=self.pageWidth, height=int(self.pageWidth/3)).generate(messages[user])
+            plt.imshow(wc, interpolation='bilinear')
+            plt.title("Wordcloud of messages sent by " + user, fontsize=30)
+            self.wordHeight += self.figureHeightPix
+        plt.tight_layout()
+        plt.savefig(new_image_dir + 'wordcloud.png', dpi=400)
+        plt.clf()
+
+    def createPieChart(self):
+        print("Creating pie chart")
+        fig = plt.figure(figsize=(self.figureWidthInches, self.figureHeightInches))
+
+        num_messages_data = self.p.get_num_messages_of_all_users()
+        users = list(num_messages_data.keys())
+        num_messages = list(num_messages_data.values())
+
+        num_words_data = self.p.get_num_words_of_all_users()
+        num_words = list(num_words_data.values())
+
+        starter_data = self.p.get_data_for_conversation_starter()
+        num_starters = list(starter_data.values())
+
+        def func(pct, allvals):
+            absolute = int(pct / 100. * np.sum(allvals))
+            return "{:.1f}%\n({:d})".format(pct, absolute)
+
+        ax = fig.add_subplot(141)
+        pie = ax.pie(num_messages, autopct=lambda pct: func(pct, num_messages), textprops=dict(color="w"))
+        ax.set_title("Messages sent", fontsize=30)
+
+        ax = fig.add_subplot(142)
+        pie = ax.pie(num_words, autopct=lambda pct: func(pct, num_words), textprops=dict(color="w"))
+        ax.set_title("Words sent", fontsize=30)
+
+        ax = fig.add_subplot(143)
+        pie = ax.pie(num_starters, autopct=lambda pct: func(pct, num_starters), textprops=dict(color="w"))
+        ax.set_title("Conversations started", fontsize=30)
+
+        ax = fig.add_subplot(144)
+        ax.axis("off")
+        ax.legend(pie[0], users, loc="center")
+
+        plt.tight_layout()
+        plt.savefig(new_image_dir + 'pie.png', dpi=400)
+        plt.clf()
+
+    def createSentimentAnalysis(self):
+        print("Creating sentiment chart")
+        sentiment = self.p.get_data_for_sentiment_chart()
+        fig, ax = plt.subplots(1, 1, figsize=(16, 9), dpi=72)
+
+        for user in sentiment:
+            y = list(sentiment[user].values())
+            x = [date2num(date) for date in list(sentiment[user].keys())]
+            plt.scatter(x, y, label=user, s=10)
+            ax.xaxis.set_major_formatter(dt.DateFormatter('%b %d, %Y'))
+
+        plt.gca().spines["top"].set_alpha(.3)
+        plt.gca().spines["bottom"].set_alpha(.3)
+        plt.gca().spines["right"].set_alpha(.3)
+        plt.gca().spines["left"].set_alpha(.3)
+
+        ax.set_xlabel('Message', fontsize=20)
+        ax.set_ylabel('Sentiment', fontsize=20)
+        ax.grid(alpha=.4)
+
+        plt.title("Sentiment analysis: [-1:1] -> [negative:positive]")
+        plt.legend()
+        #plt.savefig(new_image_dir + 'line_history.png', dpi=400)
+        #plt.clf()
+        plt.show()
+
+    def generatePDF(self):
+        scatter = new_image_dir + 'line_history.png'
+        heat = new_image_dir + 'hourly_heatmap.png'
+        word = new_image_dir + 'wordcloud.png'
+        pie = new_image_dir + 'pie.png'
+
+        pdf = PdfFileWriter()
+
+        # Using ReportLab Canvas to insert image into PDF
+        scatterHeight = self.figureHeightPix
+        pieHeight = self.figureHeightPix
+        height = scatterHeight + pieHeight + self.heatmapHeight + self.wordHeight
+        imgTemp = BytesIO()
+        imgDoc = canvas.Canvas(imgTemp, pagesize=(self.pageWidth, height))
+
+        scatterY = height - scatterHeight
+        imgDoc.drawImage(scatter, 0, scatterY, width=self.pageWidth, height=scatterHeight)
+
+        heatY = scatterY - self.heatmapHeight
+        imgDoc.drawImage(heat, 100, heatY, width=self.pageWidth, height=self.heatmapHeight)
+
+        wordY = heatY - self.wordHeight
+        imgDoc.drawImage(word, 50, wordY, width=self.pageWidth, height=self.wordHeight)
+
+        pieY = wordY - pieHeight
+        imgDoc.drawImage(pie, 50, pieY, width=self.pageWidth, height=pieHeight)
+
+        imgDoc.save()
+        pdf.addPage(PdfFileReader(BytesIO(imgTemp.getvalue())).getPage(0))
+        pdf.write(open("Conversation_analysis.pdf", "wb"))
+
+        if sys.platform == "win32":
+            os.startfile("Conversation_analysis.pdf")
+        else:
+            opener = "open" if sys.platform == "darwin" else "xdg-open"
+            subprocess.call([opener, "Conversation_analysis.pdf"])
+
+
+if __name__ == "__main__":
+    analysis = Analysis()
+    conversations = analysis.getConversations()
+    for i, conversation in enumerate(conversations):
+        print('['+ str(i) +']  ' + conversation)
+    while True:
+        chosen_index = input("Select index of chat: ")
+        try:
+            val = int(chosen_index)
+            if val < 0:  # if not a positive int print message and ask for input again
+                print("Sorry, input must be a positive integer, try again")
+                continue
+            elif val > len(conversations)-1:
+                print("Sorry, input must be smaller than the number of chats, try again")
+                continue
+            break
+        except ValueError:
+            print("Sorry, input must be an integer, try again")
+
+    while True:
+        chosen_timezone = input("Enter your timezone ('EST', 'UTC', 'Etc/GMT+1' etc.). Enter 0 for all options: ")
+        if chosen_timezone in pytz.all_timezones:
+            break
+        elif chosen_timezone == '0':
+            print(pytz.all_timezones)
+        else:
+            print("Timezone not valid, try again")
+
+    chosen = conversations[int(chosen_index)]
+
+    analysis.p = Parser(chosen, chosen_timezone)
+    analysis.createScatterChart()
+    analysis.createHeatmap()
+    analysis.createWordcloud()
+    analysis.createPieChart()
+    analysis.generatePDF()
---
+++ parse.py
@@ -0,0 +1,160 @@
+import os
+import json
+import datetime
+from collections import OrderedDict
+import pytz
+from ftfy import fix_text
+
+
+DIR = os.getcwd() + "/messages/inbox/"
+
+class Parser:
+    def __init__(self, participant, own_timezone):
+        thread_dir = DIR + participant + '/'
+        assert os.path.isdir(thread_dir), "Not a correct directory"
+        assert own_timezone in pytz.all_timezones, "Not a correct timezone"
+        self.timezone = pytz.timezone(own_timezone)
+        message_files = [thread_dir + ms_file for ms_file in os.listdir(thread_dir) if ms_file.endswith('.json')]
+        self.messages = []
+        for ms in message_files:
+            with open(ms, 'r') as read_file:
+                ms_json = json.load(read_file)
+                self.participants = [fix_text(name["name"]) for name in ms_json["participants"]]
+                self.messages += ms_json["messages"]
+        self.messages_by_user = {}
+        for user in self.participants:
+            self.messages_by_user[user] = [ms for ms in self.messages if fix_text(ms["sender_name"]) == user]
+
+    def getListOfDates(self, user):
+        timestamp_ms = [time["timestamp_ms"] for time in self.messages_by_user[user]]
+        return [datetime.datetime.fromtimestamp(ts / 1000).astimezone(self.timezone) for ts in timestamp_ms]
+
+    def get_messages_by_date_of(self, user):
+        all_messages_by_user = self.messages_by_user[user]
+        timestamp_ms = [message["timestamp_ms"] for message in all_messages_by_user]
+        dates = [datetime.datetime.fromtimestamp(ts / 1000).astimezone(self.timezone).date() for ts in timestamp_ms]
+        messages = [message.get('content') for message in all_messages_by_user]
+        return dict(zip(dates, messages))
+
+    def get_line_chart_data(self):
+        dates = {}
+        for user in self.participants:
+            days = self.getListOfDates(user)
+            days = [day.date() for day in days]
+            dates[user] = {}
+            temp = {}
+            for day in days:
+                try:
+                    temp[day] += 1
+                except KeyError:
+                    temp[day] = 1
+            dates[user] = OrderedDict(sorted(temp.items()))
+        return dates
+
+    def get_heatmap_data(self):
+        timeDay = {}
+        days = [i for i in range(7)]
+        for i, user in enumerate(self.participants):
+            times = self.getListOfDates(user)
+            by_day = []
+            total = []
+            for j, day in enumerate(days):
+                timeh = [t.hour for t in times if t.weekday() == day]
+                grouped_by_hour = [0] * 24
+                averaged_by_hour = []
+                for hour in timeh:
+                    grouped_by_hour[hour] += 1
+                for h in grouped_by_hour:
+                    if (len(timeh) != 0):
+                        averaged_by_hour.append(h/len(times))
+                    else:
+                        averaged_by_hour.append(0)
+                by_day.append(averaged_by_hour)
+            timeDay[user] = by_day
+        return timeDay
+
+
+    def get_first_5_messages(self):
+        messages = [message['content'] for message in self.messages[:5]]
+        return messages
+
+    def get_all_messages_for_wordcloud(self):
+        joined = {}
+
+        for user in self.participants:
+            messages = []
+            filtered = []
+            for ms in self.messages_by_user[user]:
+                message = ms.get('content')
+                if message is not None:
+                    messages += [fix_text(spms) for spms in message.split() if len(spms) > 5]
+
+            joined[user] = (" ").join(messages)
+        return joined
+
+    def get_num_messages_of_all_users(self):
+        num_messages = {}
+        for user in self.participants:
+            num_messages[user] = len(self.messages_by_user[user])
+        return num_messages
+
+    def get_num_messages_of(self, user):
+        num_messages = len(self.messages_by_user[user])
+        return num_messages
+
+    def get_num_words_of_all_users(self):
+        num_words = {}
+        for user in self.participants:
+            num_words[user] = 0
+            messages = [ms.get('content') for ms in self.messages_by_user[user]]
+            for message in messages:
+                if message is not None:
+                    num_words[user] += len(fix_text(message).split())
+        return num_words
+
+    def get_num_words_of(self, user):
+        num_words = 0
+        messages = [ms.get('content') for ms in self.messages_by_user[user]]
+        for message in messages:
+            if message is not None:
+                num_words += len(fix_text(message).split())
+        return num_words
+
+    def get_average_message_length_of_all_users(self):
+        avg_message_length = {}
+        for user in self.participants:
+            num_messages = self.get_num_messages_of(user)
+            num_words = self.get_num_words_of(user)
+            if num_messages is not 0:
+                avg_message_length[user] = num_words / num_messages
+            else:
+                avg_message_length[user] = 0
+        return avg_message_length
+
+    def get_average_message_length_of(self, user):
+        num_messages = self.get_num_messages_of(user)
+        num_words = self.get_num_words_of(user)
+        if num_messages is not 0:
+            avg_message_length = num_words / num_messages
+        else:
+            avg_message_length = 0
+        return avg_message_length
+
+    def get_data_for_conversation_starter(self):
+        def func(x):
+            return x.get('timestamp_ms')
+        messages = sorted(self.messages, key=func)
+
+        times_initiated_by_user = dict(zip(self.participants, [0] * len(self.participants)))
+
+        prevTimeInHours = messages[0].get('timestamp_ms') / 3600000
+        dt = 0
+        for i, content in enumerate(messages):
+            user = fix_text(content.get('sender_name'))
+            time_now = content.get('timestamp_ms') / 3600000
+            if i > 0:
+                dt = time_now - prevTimeInHours
+            if dt > 24:
+                times_initiated_by_user[fix_text(content.get('sender_name'))] += 1
+            prevTimeInHours = time_now
+        return times_initiated_by_user
